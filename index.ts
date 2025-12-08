0b3bf93e4a72f9e61021936a78613e35f73d6ce1
Lukasz Myslinski <lukasz@mlnative.com>
2025-12-08 15:45:35 +0100

    basic web app

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app
+  .post("/webhook", (c) => {
+    return c.text("Hello Hono!");
+  })
+  .get("/health", (c) => {
+    return c.text("ok");
+  });
+
+export default app;
+
